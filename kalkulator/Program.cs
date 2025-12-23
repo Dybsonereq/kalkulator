9e468cee856bc16e676fee4312d9679ebbc7f534
Dybsonereq <kuba.dybowski29@gmail.com>
2025-12-23 15:53:34 +0100

    skonczone

--- kalkulator/Program.cs
+++ kalkulator/Program.cs
@@ -1,61 +1,66 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System.Reflection.Metadata.Ecma335;
+﻿using System;
 
-Console.WriteLine("Oto mój kalkulkator");
-
-double liczbaa;
-double liczbab;
-string znak;
-bool test1, test2;
-Console.WriteLine("Podaj pierwszą liczbę");
-test1 = double.TryParse(Console.ReadLine(), out liczbaa);
-Console.WriteLine("Podaj drugą liczbę");
-test2 = double.TryParse(Console.ReadLine(), out liczbab);
-
-    Console.WriteLine("Podaj znak działania (+, -, *, /)");
-    znak = Console.ReadLine();
-if (znak == "+" || znak == "-" || znak == "*" || znak == "/")
+class Program
 {
-    switch (znak)
+    static void Main(string[] args)
     {
-        case "+":
-            {
-                Console.WriteLine("Wynik: " + (liczbaa + liczbab));
-                break;
-            }
-        case "-":
-            {
-                Console.WriteLine("Wynik: " + (liczbaa - liczbab));
-                break;
-            }
-        case "*":
-            {
-                Console.WriteLine("Wynik: " + (liczbaa * liczbab));
-                break;
-            }
-            ;
-        case "/":
-            {
-                if (liczbab != 0)
-                {
-                    Console.WriteLine("Wynik: " + (liczbaa / liczbab));
-                }
-                else
-                {
-                    Console.WriteLine("Nie można dzielić przez zero");
-                }
-                break;
-            }
-            ;
+        Console.WriteLine("Oto mój kalkulkator");
+
+        double liczbaa;
+        double liczbab;
+        string znak;
+        bool test1, test2;
+
+        Console.WriteLine("Podaj pierwszą liczbę");
+        test1 = double.TryParse(Console.ReadLine(), out liczbaa);
+        while (!test1)
+        {
+            Console.WriteLine("Podaj poprawną pierwszą liczbę");
+            test1 = double.TryParse(Console.ReadLine(), out liczbaa);
+        }
+
+        Console.WriteLine("Podaj drugą liczbę");
+        test2 = double.TryParse(Console.ReadLine(), out liczbab);
+        while (!test2)
+        {
+            Console.WriteLine("Podaj poprawną drugą liczbę");
+            test2 = double.TryParse(Console.ReadLine(), out liczbab);
+        }
+
+        Console.WriteLine("Podaj znak działania (+, -, *, /)");
+        znak = Console.ReadLine();
+
+        while (znak != "+" && znak != "-" && znak != "*" && znak != "/")
+        {
+            Console.WriteLine("Podaj prawidłowy znak działania (+, -, *, /)");
+            znak = Console.ReadLine();
+        }
+
+        Obliczenia(liczbaa, liczbab, znak);
+    }
+    private static void Obliczenia(double liczbaa, double liczbab, string znak) {
+        switch (znak)
+        {
+            case "+":
+                    Console.WriteLine("Wynik: " + (liczbaa + liczbab));
+                    break;
+            case "-":
+                    Console.WriteLine("Wynik: " + (liczbaa - liczbab));
+                    break;
+            case "*":
+                    Console.WriteLine("Wynik: " + (liczbaa * liczbab));
+                    break;
+            case "/":
+                    if (liczbab != 0)
+                        Console.WriteLine("Wynik: " + (liczbaa / liczbab));
+                    else
+                        Console.WriteLine("Nie można dzielić przez zero");
+                    break;
+        } 
     }
 }
-else
-{
-    Console.WriteLine("Nieprawidłowy znak");
-}
 
-
-//if (!test1 || !test2)
+//if (!test1 || !test2) S
 //{
 //    Console.Write("Obie muszą byc liczbami");
 //}
